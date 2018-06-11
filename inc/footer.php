8860b94f06220e14577e945a3cddf1c2f6a98c21
Harish Sharma <contact@harishsharma.net>
2018-06-11 17:21:05 +0530

    Initial Commit

--- inc/footer.php
+++ inc/footer.php
@@ -74,6 +74,7 @@
                 </ul>
             </div>
 
+            
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                 <ul class="contact">
                     <span>Website Promotion</span>    
