f264a1a94e6d56030c15e8a4bbc104842ca30d18
Mithilesh Kushwaha <mithilesh9650@gmail.com>
2018-06-22 16:16:31 +0530

    header menu change header services menu changes add training pages and modify right site content add PHP and Selenium and MIS ,Java training pages

---
+++ training/best-java-training-institute-in-delhi.php
@@ -0,0 +1,183 @@
+<?php include '../inc/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <link rel="shortcut icon" href="assets/img/favicon.ico" />
+        <meta name="viewport" content="width=device-width">
+        <title>Best Core Java Training | Core Java Training in Delhi | Core Java Training Institute in Delhi| JavaTportal</title>
+        <meta name="description" content="JavaTportal provides Core Java training In Delhi. Course duration 40 Hours with 03 live projects. Our HR Team provides 100% Job support. Trainers are having 6+ years of working experience with MNC. 400+ certified candidates Trained. Join Our Two Days free Training Program. # +91-9650322998"/>
+        <meta name="keywords" content="core java training in delhi, best core java training institute in delhi, core java training center in delhi, best core java training center in delhi, core java training institute in delhi, best core java training institutes in delhi, core java course content in delhi, core java  training in delhi, core java training delhi, core java training delhi, core java training delhi"/>
+        <link rel="canonical" href="<?php echo HOME; ?>/training/best-java-training-institute-in-delhi.php" />
+        <meta name="author" content=" javatportal.org"/>
+        <meta name="rating" CONTENT="General"/>
+        <meta name="revisit-after" CONTENT="2 days">
+        <meta name="robots" content=" ALL, index, follow"/>
+        <meta <meta name="distribution" content="Global" />
+        <meta name="rating" content="Safe ForAll" />
+        <meta name="language" content="English" />
+        <meta http-equiv="window-target" content="_top"/>
+        <meta http-equiv="pics-label" content="for all ages"/>
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Best Core Java Training in Delhi | Core Java Training Institute in Delhi – JavaTportal" />
+        <meta property="og:description" content="Get Core Java Training in Delhi by JavaTportal&#039;s corporate trainer. Best Core Java training institute in Delhi, over 4K+ Students trained. Get 100% placement assistance. Free Demo Class with 2 days free trial. CALL: +91-9650322998" />
+        <meta property="og:url" content="http://www.javatportal.org/training/best-java-training-institute-in-delhi.php" />
+        <meta property="og:site_name" content="JavaTportal" />
+        <meta property="article:publisher" content="https://www.facebook.com/javatportal/" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:description" content="Get Core Java Training in Delhi by JavaTportal&#039;s corporate trainer. Best Core Java training institute in Delhi, over 4K+ Students trained. Get 100% placement assistance. Free Demo Class with 2 days free trial. CALL: +91-9650322998" />
+        <meta name="twitter:title" content="Best Core Java Training in Delhi | Core Java  Training Institute/Center in Delhi – JavaTportal" />
+        <meta name="twitter:site" content="@javatportal" />
+        <meta name="twitter:creator" content="@javatportal" />
+        <meta content="All, FOLLOW" name="GOOGLEBOTS"/>
+        <meta content="All, FOLLOW" name="YAHOOBOTS"/>
+        <meta content="All, FOLLOW" name="MSNBOTS"/>
+        <meta content="All, FOLLOW" name="BINGBOTS"/>
+        <meta content="all" name="Google bot -Image"/>
+        <meta content="ALL" name="WEBCRAWLERS"/>
+        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1"/>
+
+        <?php include '../styling.php'; ?>
+        <style>
+            h1, h2, h3, h4, h5, h6 
+            {
+                text-align: center !important;
+            }
+            .row .col{
+                text-align:center;
+            }
+        </style>
+        <title></title>
+    </head>
+    <body>
+        <?php include '../inc/header.php'; ?>
+        <style>
+            .jumbotron {
+                padding: 2rem 1rem;
+                margin-bottom: 2rem;
+                background-color: #fafafa;
+                border-radius: 0.3rem;
+                background-image: url(<?php echo HOME; ?>/assets/img/jumborton.jpg);
+            }
+
+        </style>
+        <div class="jumbotron">
+            <div class="opacity-effect">
+                <h1 class="display-3"><i class="fas fa-database"></i>&nbsp;Core Java Training Institute in Delhi</h1>
+                <h4>JavaTportal Corporation offers best Core Java Training in Delhi with most experienced professionals</h4>
+                <h5><i class="fas fa-star icogreen"></i><i class="fas fa-star icogreen"></i><i class="fas fa-star icogreen"></i><i class="fas fa-star icogreen"></i><i class="fas fa-star icogreen"></i>9325 Ratings</h5>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h2>Welcome to the Best Core Java Training Institute in Delhi</h2>
+                    <img src="../assets/img/training/best-training-institute.jpg" width="100%"> <br><br> 
+                    <p>JavaTportal Corporation offers <strong>Best Core Java Training in Delhi</strong> with most experienced professionals. We're a leading <strong>Core Java training institute in Delhi.</strong> We provide most learning environment for major technical course at affordable price Our Instructors are working in Core Java and related technologies for more years in MNC's. We aware of industry needs and we are offering <strong>Core Java Training in Delhi</strong>  in more practical way.
+                        Our team of Core Java trainers offers Core Java in Classroom training,<strong> Core Java Online Training and Core Java Corporate Training services</strong>. We framed our syllabus to match with the real world requirements for both beginner level to advanced level. Our training will be handled in either weekday or weekends programmer depends on participants requirement.
+                    </p>
+                    <h3>Best Core Java Training in Delhi</h3>
+                    <p>JavaTportal <strong> Core Java Certification Training </strong> will help you in mastering the various concepts of Core Java from scratch. JavaTportal trainers provides highly customized and result oriented training in Core Java, Advance Java, Struts 2.x, Spring, Hibernate, Web Services etc. We focus on practical training sessions as well as theoretical classes.</p>  
+                    <p>We do offer Fast-Track <strong>Core Java Training in Delhi</strong> and One-to-One Core Java Training in Delhi. Here are the major topics we cover under this Core Java course Syllabus . Every topic will be covered in mostly practical way with examples. JavaTportal Corporation located in various places in Delhi. We are the <strong> Best Training Institute in Delhi</strong> offers certification oriented Core Java Training in Delhi. Our participants will be eligible to clear all type of interviews at end of our sessions. We are building a team of Core Java trainers and participants for their future help and assistance in subject. Our training will be focused on assisting in placements as well. We have separate HR team professionals who will take care of all your interview needs. Our <strong>Core Java Training in Delhi</strong> Course Fees is very moderate compared to others. We are the only <strong>Core Java training institute in Delhi </strong> who can share video reviews of all our students. We mentioned the course timings and start date as well in below.</p>
+                    <p>JavaTportal is the well-known <strong> Core Java Training Center in Delhi </strong>with high tech infrastructure and lab facilities. We also provide online access of servers so that candidates will implement the projects at their home easily. JavaTportal in Delhi mentored more than 4000+ candidates with <strong>Core Java Certification Training in Delhi </strong> at very reasonable fee. The course curriculum is customized as per the requirement of candidates/corporate.</p>
+                    <h2><p class="bg-primary"><i class="fas fa-hand-point-right"></i> Placement Assistance after Core Java Training in Delhi</p></h2>
+                    <ul>
+                        <li> JavaTportal Top Placement Company are :- <strong> HCL, Wipro, Dell, Birlasoft, Tech Mahindra, TCS, IBM</strong></li>
+                        <li> JavaTportal HR team focuses on Group Discussions, personality development, and create students RESUME as per current company standards.</li>
+                        <li> After completion of 75% training course content, we will arrange the interview calls to students & prepare them to F2F interaction.</li>
+                        <li> CV Designing as per the company standard  (Job Description).</li>
+                        <li>Better Aptitude & Test Papers For Candidates by trainer.</li>
+                        <li> We Prepare HR Interview.</li>
+                        <li> We provide Job Portal for Credentials so that Candidates can apply the job.</li>
+                        <li>We provide 100% placement assistance</li>
+                        <li> We provides interviews calls till Placement.</li>
+                    </ul>
+
+                    <h3>What is Core Java</h3>
+                      <p>Java is a <strong>programming language</strong> and a <strong>platform</strong>.</p>
+<p>Java is a high level, robust, secured and object-oriented programming language.</p>
+<p><strong>Platform</strong>: Any hardware or software environment in which a program runs, is known as a platform. Since Java has its own runtime environment (JRE) and API, it is called platform.</p>               
+                    <div class="row"> 
+
+                    </div>
+                    <hr/>       
+                    <div id="accordion">
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h8 class="mb-0"  text-align: left>
+                                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        <i class="fas fa-chevron-circle-down"></i> Core Java Trainer Profile 
+                                    </button>
+                                </h8>
+                            </div>
+
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                                <div class="card-body">
+                                    <ul> 
+                                        <li>More than 10 Years of experience in Core Java&reg; Technologies</li>
+                                        <li>Has worked on multiple realtime Core Java projects</li>
+                                        <li>Working in a top MNC company in Bangalore</li>
+                                        <li>Trained 2000+ Students so far</li>
+                                        <li>Strong Theoretical & Practical Knowledge</li>
+                                        <li>Certified Professionals</li>
+                                    </ul>      </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingTwo">
+                                <h8 class="mb-0">
+                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        <i class="fas fa-chevron-circle-down"></i>   Core Java Placement Training in Delhi
+                                    </button>
+                                </h8>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                <div class="card-body">
+                                    <ul> 
+                                        <li>    More than 2000+ students Trained</li>
+                                        <li> 92% percent Placement Record</li>
+                                        <li> 1000+ Interviews Organized</li></ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingThree">
+                                <h8 class="mb-0">
+                                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        <i class="fas fa-chevron-circle-down"></i>   Core Java Training Batch Size in Delhi
+                                    </button>
+                                </h8>
+                            </div>
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                <div class="card-body">
+                                    <div class="card-body"> 
+
+                                        <ul class="list-1">
+                                            <strong>Regular Batch (Morning, Day time &amp; Evening)</strong>
+                                            <li>Seats Available : 10 (maximum)</li>
+                                        </ul>
+                                        <ul class="list-1">
+                                            <strong>Weekend Training Batch (Saturday, Sunday &amp; Holidays)</strong>
+                                            <li>Seats Available : 8 (maximum)</li>
+                                        </ul>
+                                        <ul class="list-1">
+                                            <strong>Fast Track Batch</strong>
+                                            <li>Seats Available : 5 (maximum)</li>
+                                        </ul>
+                                    </div> 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <?php include '../inc/oralce-related-courses.php' ?>
+                </div>   
+                <div class="col-md-4">
+                    <?php include '../inc/contact-sidebar.php' ?>
+                    <?php include '../inc/sidebar-course-features.php' ?>
+
+
+                </div>
+            </div>
+        </div> 
+        <?php include '../inc/footer.php'; ?>   
