66587aef10d1c26321a8c764b1c272792d8cdcc8
Harish Sharma <contact@harishsharma.net>
2018-06-24 20:57:56 +0530

    New Changes

--- inc/header.php
+++ inc/header.php
@@ -6,8 +6,62 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="http://blog.bootswatch.com">About Us</a>
+                </li>
+                
+                
+                
+                
+                <!-- ----------------------------MULTI LEVEL DROP DOWN START ------------------------------>
+                <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#">Dropdown&nbsp;<i class="fa fa-caret-down"></i></a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item disabled" href="#">Disabled link</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li class="dropdown-divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a class="dropdown-item" href="#">Separated link</a></li>
+                <li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">One more separated link&nbsp;<i class="fa fa-caret-right"></i></a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li class="dropdown"><a class="dropdown-item dropdown-toggle" href="#">A long sub menu&nbsp;<i class="fa fa-caret-right"></i></a>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">One more link</a></li>
+                        <li><a class="dropdown-item" href="#">Menu item 1</a></li>
+                        <li><a class="dropdown-item" href="#">Menu item 2</a></li>
+                        <li><a class="dropdown-item" href="#">Menu item 3</a></li>
+                        <li><a class="dropdown-item" href="#">Menu item 4</a></li>
+                        <li><a class="dropdown-item" href="#">Menu item 5</a></li>
+                      </ul>
+                    </li>
+                    <li><a class="dropdown-item" href="#">Another link</a></li>
+                    <li><a class="dropdown-item" href="#">One more link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <!-- ----------------------------MULTI LEVEL DROP DOWN ENDS ------------------------------>
+            
+            
+            
+            
+            
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="themes">All Courses <span class="caret"></span></a>
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Services <span class="caret"></span></a>
+                    <div class="dropdown-menu" aria-labelledby="themes">
+                        <!--    <a class="dropdown-item" href="../default/"></a>
+                            <div class="dropdown-divider"></div> -->
+                        <a class="dropdown-item" href="../cerulean/">Web Development</a>
+                        <a class="dropdown-item" href="../cosmo/">Web Development</a>
+                        <a class="dropdown-item" href="../cyborg/">Web Marketing</a>
+                                    
+                    </div>
+                </li>      
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Training Programme <span class="caret"></span></a>
                     <div class="dropdown-menu" aria-labelledby="themes">
                         <!--    <a class="dropdown-item" href="../default/"></a>
                             <div class="dropdown-divider"></div> -->
@@ -24,42 +78,41 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../help/">Latest Placements</a>
+                    <a class="nav-link" href="../help/">Placements</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://blog.bootswatch.com">Clients</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Services <span class="caret"></span></a>
-                    <div class="dropdown-menu" aria-labelledby="themes">
-                        <!--    <a class="dropdown-item" href="../default/"></a>
-                            <div class="dropdown-divider"></div> -->
-                        <a class="dropdown-item" href="../cerulean/">Web Development</a>
-                        <a class="dropdown-item" href="../cosmo/">Web Development</a>
-                        <a class="dropdown-item" href="../cyborg/">Web Marketing</a>
-                    </div>
+                    <a class="nav-link" href="http://blog.bootswatch.com">Packages</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="http://blog.bootswatch.com">Our Packages</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="http://blog.bootswatch.com">About Us</a>
-                </li>
-                 <li class="nav-item">
-                    <a class="nav-link" href="http://blog.bootswatch.com"> Apply as a Trainer</a>
-                </li>
-                 <li class="nav-item">
                     <a class="nav-link" href="http://blog.bootswatch.com">Reviews</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="http://blog.bootswatch.com">Contact Us</a>
                 </li>
-            </ul>
-
-            <ul class="nav navbar-nav ml-auto">
-
+            <li class="nav-item">
+                    <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">Apply as Trainer</button>
+                </li>
             </ul>
         </div>
     </div>
 </div>
-<br/><br/>
+
+
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Application for Trainer Profile</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
--- styling.php
+++ styling.php
@@ -19,5 +19,11 @@
   <script src="https://bootswatch.com/_vendor/jquery/dist/jquery.min.js"></script>
   <script src="https://bootswatch.com/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
   <script src="https://bootswatch.com/_vendor/popper.js/dist/umd/popper.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script type="text/javascript" src="http://localhost/javatportal.org/assets/js/jquery.smartmenus.js"></script>
+    <!-- SmartMenus jQuery Bootstrap 4 Addon -->
+    <script type="text/javascript" src="http://localhost/javatportal.org/assets/js/jquery.smartmenus.bootstrap-4.js"></script>
+
+
   
   
