c586a3d9cf0093525d3c83a4638994facb4edbdc
Harish Sharma <contact@harishsharma.net>
2018-06-30 10:15:32 +0530

    New Changes

--- styling.php
+++ styling.php
@@ -8,6 +8,6 @@
  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
 <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">-->
 
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery-3.2.1.slim.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
