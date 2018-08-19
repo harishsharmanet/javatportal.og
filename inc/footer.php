6d3c6e88a39a3584848493b7cd78d70792eef723
mithileshkushwaha <mithilesh9650@gmail.com>
2018-08-19 22:20:39 +0530

    update right bar update

--- inc/footer.php
+++ inc/footer.php
@@ -35,7 +35,27 @@
     </div>
 </section>
 <footer>
-    <div class="container"> <!-- Container Class Missing -->
+     <div class="container"> <!-- Container Class Missing -->
+        <div class="row">
+ 
+     <!-- Container Class Missing -->
+        <script src=newsletter_subscription.js></script><div class=course-footer>
+            <div class=footer-courses><div class=container><div class="col-md-12 footer-heading">
+                        <h3>Top Courses</h3></div><div class=col-md-12>
+                        <div class="col-md-3 col-sm-6">
+                            <div class=course-col><ul><li><b>> </b><a href=https://www.javatportal.org/core-java.php>Core Java/J2SE</a><li><b>> </b><a href=https://www.javatportal.org/advance-java.php>Advance Java/J2EE</a><li><b>> </b><a href=https://www.javatportal.org/java-frameworks.php>Java Frameworks</a><li><b>> </b><a href=https://www.javatportal.org/API-Development-Training.php>Web Services (API Development)</a><li><b>> </b><a href=https://www.javatportal.org/dot-net.php>.NET (Basic|Advance)</a><li><b>> </b><a href=https://www.javatportal.org/solidworks.php>SolidWorks</a><li><b>> </b><a href=https://www.javatportal.org/mvc50.php>MVC Framework</a><li><b>> </b><a href=https://www.javatportal.org/asp-net.php>ASP .NET</a><li><b>> </b><a href=https://www.javatportal.org/courses/c-c-training/index.html>C & C++</a><li><b>> </b><a href=https://www.javatportal.org/swift.php>Swift</a><li><b>> </b><a href=https://www.javatportal.org/html-css.php>HTML5 & CSS3</a></ul></div>
+                        </div>
+                        <div class="col-md-3 col-sm-6"><div class=course-col><ul><li><b>> </b><a href=https://www.javatportal.org/webd.php>Web Designing</a><li><b>> </b><a href=https://www.javatportal.org/php-mysql.php>PHP/MySQL (Core + Advance)</a><li><b>> </b><a href=https://www.javatportal.org/php-frameworks.php>PHP Frameworks</a><li><b>> </b><a href=https://www.javatportal.org/angularjs.php>AngularJs</a><li><b>> </b><a href=https://www.javatportal.org/python.php>Python</a><li><b>> </b><a href=https://www.javatportal.org/robotics.php>Robotics</a><li><b>> </b><a href=https://www.javatportal.org/verilog.php>Verilog</a><li><b>> </b><a href=https://www.javatportal.org/salesforce-admin.php>Salesforce Admin-201</a><li><li><b>> </b><a href=https://www.javatportal.org/etl-testing.php>ETL Testing</a><li><b>> </b><a href=https://www.javatportal.org/db-testing.php>Database Testing</a><li><b>> </b><a href=https://www.javatportal.org/mb-testing.php>Mobile Application Testing</a></ul></div>
+                        </div>
+                        <div class="col-md-3 col-sm-6"><div class=course-col><ul><li><b>> </b><a href=https://www.javatportal.org/agile.php>Agile</a><li><b>> </b><a href=https://www.javatportal.org/selenium.php>Selenium</a><li><b>> </b><a href=https://www.javatportal.org/loadrunner.php>LoadRunner</a><li><b>> </b><a href=https://www.javatportal.org/jmeter.php>Jmeter</a><li><b>> </b><a href=https://www.javatportal.org/coded-UI.php>VSTS – CodedUI</a><li><b>> </b><a href=https://www.javatportal.org/appium.php>Appium</a><li><b>> </b><a href=https://www.javatportal.org/dba-11g-12c.php>Oracle DBA (10G|11G|12C)</a><li><b>> </b><a href=https://www.javatportal.org/sql-server-dba.php>SQL Server DBA (2005|08|12|14)</a><li><b>> </b><a href=https://www.javatportal.org/sql-server-dev.php>SQL Server Developer</a><li><b>> </b><a href=https://www.javatportal.org/matlab.php>Matlab Training in Noida</a><li><b>> </b><a href=https://www.javatportal.org/beaglebone.php>Beaglebone</a></ul></div></div>
+                        <div class="col-md-3 col-sm-6"><div class=course-col><ul><li><b>> </b><a href=https://www.javatportal.org/android.php>Android</a><li><b>> </b><a href=https://www.javatportal.org/ios.phpN>iOS (I-Phone)</a><li><b>> </b><a href=https://www.javatportal.org/phonegap.php>PhoneGap</a><li><b>> </b><a href=https://www.javatportal.org/networking.php>Networking</a><li><b>> </b><a href=https://www.javatportal.org/ccna.php>(CCNA)</a><li><b>> </b><a href=https://www.javatportal.org/aruduino.php>Arduino</a><li><b>> </b><a href=https://www.javatportal.org/coral-draw.php>Coral Draw</a><li><b>> </b><a href=https://www.javatportal.org/photoshop.php>Adobe Photoshop</a><li><b>> </b><a href=https://www.javatportal.org/indesign.php>Adobe Indesign</a><li><b>> </b><a href=https://www.javatportal.org/illustrator.php>Adobe Illustrator</a><li><b>> </b><a href=https://www.javatportal.org/hadoop-admin.php>Hadoop Admin</a></ul></div>
+                        </div></div></div></div>
+            <div class=footer-top><div class=container><div class=col-md-12><div class="col-md-3 col-sm-6 ftcontent"><h3>Follow Us !</h3><div class=social><ul><li><a href=https://www.facebook.com/10daneces/ ><i class="fab fa-facebook-square"></i></a></li><li><a href=https://www.linkedin.com/company/10daneces/ ><i class="fab fa-linkedin"></i></a></li><li><a href=https://plus.google.com/104388538276764236262><i class="fab fa-google-plus-square"></i></a></li><li><a href=https://twitter.com/p2psystem><i class="fab fa-twitter-square"></i></a></li><li><a href=https://www.youtube.com/channel/UCBgf9e2p-EtOIOESbmY5a2A><i class="fab fa-youtube-square"></i></a></li></ul></div></div><div class="col-md-3 col-sm-6 ftcontent"><h3>Subscribe to our Newsletter</h3><div class=newsletter><form action=""name=""><div class=letterinp><input class=emailn id=newsletter_email name=email-for-newsletter placeholder="Enter Your Email Address"style=margin-top:-1px type=email> <span class=subbutton style=margin-left:-4px><button id=newsletter_button style=border-radius:0;height:100%;background-color:#f7902d;border:none type=submit><i class="fa-paper-plane fas"></i></button></span></div></form>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-6 ftcontent"><h3>We Accept Online Payments</h3><div class=onlinepayment><a href=""><img src=assets/img/pay-option.jpg></a></div></div><div class="col-md-3 col-sm-6 ftcontent"><h3>Learn On</h3><div class=onlinepayment><a href=""><img src=assets/img/google-play.jpg></a></div></div></div><div class=col-md-12><div class="container bottom-nav"><span><a href=aboutus.php>About Us </a></span><span><a href=contactus.php>Contact Us </a></span><span><a href=career.php>Career </a></span><span><a href=request_certificate.php>Apply Training Certificate </a></span><span><a href=feedback.php>Feedback</a></span></div></div></div></div></div>
+ 
+    <div class="container"> <!-- Container Class Missing  
         <div class="row">
 
             <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
@@ -120,7 +140,7 @@
             </div>
 
 
-        </div> 
+        </div>  -->
     </div>
       <div class="container">
           <center><img src="http://localhost/javatportal.org/assets/img/jtp_logo.png" class="img-responsive">
